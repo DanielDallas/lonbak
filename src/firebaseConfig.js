363899f9ba07dcdf24a8c18c382b5a4819709e5c
Daniel Dallas <thedanieldallas@gmail.com>
2023-06-07 16:23:34 +0000

    More initial modification

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,22 @@
-import firebase from "firebase/app"
-imports "firebase/auth"
+// firebaseConfig.js
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyA-ZhwyIvH281vwUDwMg7H10LxzWies41M",
+  authDomain: "lonbak-app.firebaseapp.com",
+  projectId: "lonbak-app",
+  storageBucket: "lonbak-app.appspot.com",
+  messagingSenderId: "642322408518",
+  appId: "1:642322408518:web:2babb32d8fd547d13ad647",
+  measurementId: "G-N6HPEWJXGP"
+};
 
-    apiKey: process.env.REACT_APP_FIERBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIERBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIERBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIERBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIERBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIERBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIERBASE_MEASUREMENT_ID
-  };
-  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
